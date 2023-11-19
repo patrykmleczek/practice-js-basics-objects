f0a5d18de728d40e3c59ebe3a8afd886694b2809
Patryk Mleczek <145116649+patrykmleczek@users.noreply.github.com>
2023-11-19 21:21:41 +0100

    solve exercise 01

---
+++ 01/app.js
@@ -0,0 +1,11 @@
+const user = {
+    firstName: "Joe",
+    lastName: "Doe",
+    sex: "male",
+    age: 30
+}
+
+console.log(user.firstName);
+console.log(user.lastName);
+console.log(user.sex);
+console.log(user.age);
